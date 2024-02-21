c0c0b6cf37f2f65db1a119aecbc7c94882097b86
shcherbakova.sa@phystech.edu <shcherbakova.sa@phystech.edu>
2024-02-21 21:31:07 +0300

    A~A

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication2.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication2.cpp
@@ -14,11 +14,11 @@ int func(int a[], unsigned N, int x)
 	while ((l <= r) and (flag == false)) 
 	{
 		m = (l + r) / 2;
-		if (arr[m] == x)
+		if (a[m] == x)
 		{
 			flag = true;
 		}
-		if (arr[m] > x)
+		if (a[m] > x)
 		{
 			r = m - 1;
 		}
@@ -27,6 +27,7 @@ int func(int a[], unsigned N, int x)
 			l = m + 1;
 		}
 	}
+	return m;
 }
 void Sortirovochka(int a[], unsigned N) {
 	for (unsigned i = 0; i < N - 1; i++) {
@@ -56,6 +57,8 @@ int main()
 		nabiv_massivchika(a, N, 1'000'000'000);
 		Sortirovochka(a, N);
 		nabiv_massivchika(x, 1, N);
+		cout << a[0] << " " << a[1] << " " << a[2] << " " << a[3] << " " << a[4] << " " << a[5] << endl;
+		cout << a[x[0]];
 		auto begin = chrono::steady_clock::now();
 		for (unsigned cnt = 100000; cnt != 0; --cnt)
 		{
@@ -64,6 +67,6 @@ int main()
 		auto end = chrono::steady_clock::now();
 		auto time_span =
 			chrono::duration_cast<chrono::milliseconds>(end - begin);
-		cout << time_span.count() << " ";
+		//cout << time_span.count() << " ";
 	}
 }
